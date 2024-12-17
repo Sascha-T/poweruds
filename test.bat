16332f4218f36c3e267ee0c8fd7271df66c53377
Sascha T. <info@sascha-t.de>
2024-12-17 22:00:22 +0100

    how am i allowed to even exist
    i should be locked up
    timeout=0????

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 (Echo open_session
 Echo change_com_line^|17
 Echo bind_protocol^|0310E8
-Echo write_and_read^|06520752^|1003^|0) > tmp.txt
+Echo write_and_read^|06520752^|1003^|1000) > tmp.txt
 type tmp.txt | vciproxy
