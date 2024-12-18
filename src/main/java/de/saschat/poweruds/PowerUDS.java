29f25b4c3657ee2366ca8f1d57845ab1d4fd94b7
Sascha T. <info@sascha-t.de>
2024-12-18 17:16:21 +0100

    bluetooth update final test?

--- src/main/java/de/saschat/poweruds/PowerUDS.java
+++ src/main/java/de/saschat/poweruds/PowerUDS.java
@@ -2,6 +2,7 @@ package de.saschat.poweruds;
 
 import de.saschat.poweruds.adapter.AbstractAdapter;
 import de.saschat.poweruds.base.adapters.DiagboxAdapter;
+import de.saschat.poweruds.base.commands.UnlockCommand;
 import de.saschat.poweruds.cli.PUDSCLI;
 import de.saschat.poweruds.spi.ExtensionProvider;
 
@@ -9,8 +10,12 @@ import java.util.*;
 
 public class PowerUDS {
     private static final ServiceLoader<ExtensionProvider> LOADER = ServiceLoader.load(ExtensionProvider.class);
+
     public static void main(String[] args) {
-        new PowerUDS().startCLI(args);
+        byte[] seed = new byte[] {0x0D, 0x16, 0x61, 0x46};
+        byte[] key = new byte[] {(byte) 0xB4, (byte) 0xE0};
+        System.out.println(UnlockCommand.compute(key, seed));
+        // new PowerUDS().startCLI(args);
     }
     private PUDSCLI cli;
     public PowerUDS() {
--- src/main/java/de/saschat/poweruds/base/adapters/ELM327Adapter.java
+++ src/main/java/de/saschat/poweruds/base/adapters/ELM327Adapter.java
@@ -5,13 +5,25 @@ import de.saschat.poweruds.adapter.AdapterInitializationException;
 import de.saschat.poweruds.cli.PUDSCLI;
 
 import javax.bluetooth.*;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class ELM327Adapter extends AbstractAdapter implements DiscoveryListener {
     private final Object lock = new Object();
 
+    private ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
     private static javax.bluetooth.UUID[] SERVICES = new javax.bluetooth.UUID[]{new javax.bluetooth.UUID("0000110100001000800000805F9B34FB", false)};
     private static LocalDevice DEVICE;
     private static DiscoveryAgent AGENT;
@@ -19,6 +31,10 @@ public class ELM327Adapter extends AbstractAdapter implements DiscoveryListener 
     private static boolean initialized = false;
     private boolean connected = false;
     private RemoteDevice selected;
+    private StreamConnection connection;
+    private DataInputStream in;
+    private DataOutputStream out;
+
 
     static {
         try {
@@ -30,8 +46,15 @@ public class ELM327Adapter extends AbstractAdapter implements DiscoveryListener 
         }
     }
 
+    private static final String[] INIT_COMMANDS = new String[] {
+            "ATZ", "ATWS", "ATD", "ATE0", "ATL0", "ATH0", "ATS0", "ATAL", "ATV0", "ATSP6"
+    };
+
     public Map<String, RemoteDevice> DEVICES = new HashMap<>();
     public Map<RemoteDevice, String> AVAILABLE = new HashMap<>();
+
+    public Queue<Character> IN_QUEUE = new ArrayBlockingQueue<>(1024);
+    public Thread READ_QUEUE;
 
     @Override
     public String getName() {
@@ -46,17 +69,100 @@ public class ELM327Adapter extends AbstractAdapter implements DiscoveryListener 
             throw new RuntimeException("You need to specify device name or address.");
         }
 
-        if (!inquire(true))
-            return;
         RemoteDevice dev;
-        if ((dev = getDevice(options)) != null)
-            if (connect(dev))
+        boolean dnq = (dev = getDevice(options)) != null;
+        if (!dnq && !inquire(true))
+            return;
+        if (dev != null || (dev = getDevice(options)) != null)
+            if (connect(dev)) {
+                for (String initCommand : INIT_COMMANDS) { // initialize state
+                    if(writeAndRead(initCommand).equals("?"))
+                        return;
+                }
                 initialized = true;
+            }
     }
 
     public boolean connect(RemoteDevice device) {
+        if(AVAILABLE.get(device) == null)
+            inquireSpecific(device);
         // connect
+        try {
+            connection = (StreamConnection) Connector.open(AVAILABLE.get(device));
+            in = connection.openDataInputStream();
+            out = connection.openDataOutputStream();
+
+            READ_QUEUE = new Thread(() -> {
+                try {
+                    while(true) {
+                        char read = in.readChar();
+                        while(!IN_QUEUE.add(read)) {
+                            Thread.sleep(100);
+                        }
+                    }
+                } catch (Throwable e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+            connected = true;
+            return true;
+        } catch (IOException e) {
+            PUDSCLI.exception(e);
+        }
         return false;
+    }
+
+    public void writeCommand(String text) {
+        try {
+            for (char c : text.toCharArray()) {
+                out.write(c);
+            }
+            out.write('\n');
+        } catch (IOException e) {
+            PUDSCLI.exception(e);
+            throw new RuntimeException(e);
+        }
+    }
+    public String readReply() {
+        StringBuilder text = new StringBuilder();
+        try {
+            for (int i = 0; i < 20; i++) {
+                if(!IN_QUEUE.isEmpty()) {
+                    if(IN_QUEUE.contains('\n')) {
+                        // consume one line
+
+                        Character character = null;
+                        while((character = IN_QUEUE.remove()) != null && character != '\n')
+                            text.append(character);
+
+                        return text.toString();
+                    }
+                }
+                Thread.sleep(100);
+            }
+            return "";
+        } catch (Throwable e) {
+            PUDSCLI.exception(e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String writeAndRead(String text) {
+        writeCommand(text);
+        return readReply();
+    }
+
+    private void inquireSpecific(RemoteDevice device) {
+        try {
+            selected = device;
+            AGENT.searchServices(null, SERVICES, selected, this);
+            synchronized (lock) {
+                lock.wait();
+            }
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean inquire(boolean doShort) {
@@ -70,11 +176,7 @@ public class ELM327Adapter extends AbstractAdapter implements DiscoveryListener 
                 return true;
 
             for (RemoteDevice value : DEVICES.values()) {
-                selected = value;
-                AGENT.searchServices(null, SERVICES, selected, this);
-                synchronized (lock) {
-                    lock.wait();
-                }
+                inquireSpecific(value);
             }
             selected = null;
 
@@ -103,12 +205,26 @@ public class ELM327Adapter extends AbstractAdapter implements DiscoveryListener 
 
     @Override
     public void selectECU(String code) {
+        String[] split = code.split(":");
+        String txh = split[0];
+        String rxh = split[1];
 
+        String resp;
+        if((resp = writeAndRead("ATSH" + txh)).equals("?"))
+            throw new RuntimeException("Failed changing TXH (1)");
+        if((resp = writeAndRead("ATCRA" + rxh)).equals("?"))
+            throw new RuntimeException("Failed changing RXH (1)");
+        if((resp = writeAndRead("ATFCSH" + txh)).equals("?"))
+            throw new RuntimeException("Failed changing TXH (2)");
+        if((resp = writeAndRead("ATFCSD300000")).equals("?"))
+            throw new RuntimeException("Failed changing flow control data");
+        if((resp = writeAndRead("ATFCSM1")).equals("?"))
+            throw new RuntimeException("Failed changing flow control mode");
     }
 
     @Override
     public Future<String> sendUDS(String uds) {
-        return null;
+        return exec.submit(() -> writeAndRead(uds));
     }
 
     @Override
