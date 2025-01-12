3505a7ca69ee3862433c2e23f906d1a7ecb4cfb5
Sascha T. <info@sascha-t.de>
2025-01-12 23:40:12 +0100

    less cursed

--- src/main/java/de/saschat/poweruds/base/adapters/ELM327Adapter.java
+++ src/main/java/de/saschat/poweruds/base/adapters/ELM327Adapter.java
@@ -8,14 +8,11 @@ import javax.bluetooth.*;
 import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
 import java.io.DataInputStream;
-import java.io.DataOutput;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
@@ -35,6 +32,9 @@ public class ELM327Adapter extends AbstractAdapter implements DiscoveryListener 
     private DataInputStream in;
     private DataOutputStream out;
 
+    public String selectedECUrx = "";
+    public String selectedECUtx = "";
+
 
     static {
         try {
@@ -46,15 +46,23 @@ public class ELM327Adapter extends AbstractAdapter implements DiscoveryListener 
         }
     }
 
-    private static final String[] INIT_COMMANDS = new String[] {
-            "ATZ", "ATWS", "ATD", "ATE0", "ATL0", "ATH0", "ATS0", "ATAL", "ATV0", "ATSP6"
+    private record InitCommand(String command, boolean echo) {};
+    private static final InitCommand[] INITIALIZATION = new InitCommand[] {
+            new InitCommand("AT Z", true), // reset
+            new InitCommand("AT D", true), // all to default
+            new InitCommand("AT E 0", false), // disable echo
+            new InitCommand("AT E 0", false), // disable echo
+            new InitCommand("AT L 0", false), // disable echo
+            new InitCommand("AT H 0", false), // disable echo
+            new InitCommand("AT S 0", false), // disable echo
+            new InitCommand("AT AL", false), // disable echo
+            new InitCommand("AT V 0", false), // disable echo
+            new InitCommand("AT SP 6", false), // disable echo
     };
 
     public Map<String, RemoteDevice> DEVICES = new HashMap<>();
     public Map<RemoteDevice, String> AVAILABLE = new HashMap<>();
 
-    public Queue<Character> IN_QUEUE = new ArrayBlockingQueue<>(1024);
-    public Thread READ_QUEUE;
 
     @Override
     public String getName() {
@@ -69,18 +77,44 @@ public class ELM327Adapter extends AbstractAdapter implements DiscoveryListener 
             throw new RuntimeException("You need to specify device name or address.");
         }
 
+        readPaired();
         RemoteDevice dev;
         boolean dnq = (dev = getDevice(options)) != null;
         if (!dnq && !inquire(true))
             return;
-        if (dev != null || (dev = getDevice(options)) != null)
-            if (connect(dev)) {
-                for (String initCommand : INIT_COMMANDS) { // initialize state
-                    if(writeAndRead(initCommand).equals("?"))
-                        return;
+        try {
+            if (dev != null || (dev = getDevice(options)) != null)
+                if (connect(dev)) {
+                    for (InitCommand initCommand : INITIALIZATION) { // initialize state
+                        if (initCommand.echo()) { // yes echo command
+                            String ret;
+                            readPrompt();
+                            if ((ret = safeWriteAndRead(initCommand.command()).get()).equals(initCommand.command())) {
+                                System.out.println("D1: " + ret); // debug
+                            } else {
+                                throw new RuntimeException("command was not echoed: " + ret); // todo better
+                            }
+                        } else safeWriteAndIgnore(initCommand.command());
+
+                        String ret = safeRead().get();
+                        if(ret.equals("?")) {
+                            throw new RuntimeException("command not understood: " + initCommand);
+                        }
+                    }
+                    initialized = true;
                 }
-                initialized = true;
-            }
+        } catch (Exception ex) {
+            PUDSCLI.exception(ex);
+            initialized = false;
+        }
+    }
+
+    private void readPaired() {
+        var dev = DEVICE.getDiscoveryAgent().retrieveDevices(DiscoveryAgent.PREKNOWN);
+        for (RemoteDevice remoteDevice : dev) {
+            DEVICES.put(remoteDevice.getBluetoothAddress(), remoteDevice);
+            inquireSpecific(remoteDevice);
+        }
     }
 
     public boolean connect(RemoteDevice device) {
@@ -92,19 +126,6 @@ public class ELM327Adapter extends AbstractAdapter implements DiscoveryListener 
             in = connection.openDataInputStream();
             out = connection.openDataOutputStream();
 
-            READ_QUEUE = new Thread(() -> {
-                try {
-                    while(true) {
-                        char read = in.readChar();
-                        while(!IN_QUEUE.add(read)) {
-                            Thread.sleep(100);
-                        }
-                    }
-                } catch (Throwable e) {
-                    throw new RuntimeException(e);
-                }
-            });
-
             connected = true;
             return true;
         } catch (IOException e) {
@@ -113,47 +134,76 @@ public class ELM327Adapter extends AbstractAdapter implements DiscoveryListener 
         return false;
     }
 
-    public void writeCommand(String text) {
+    public void btWrite(String text) {
         try {
             for (char c : text.toCharArray()) {
                 out.write(c);
             }
             out.write('\n');
         } catch (IOException e) {
+            connected = false;
             PUDSCLI.exception(e);
             throw new RuntimeException(e);
         }
     }
-    public String readReply() {
+    public String btRead() {
         StringBuilder text = new StringBuilder();
         try {
-            for (int i = 0; i < 20; i++) {
-                if(!IN_QUEUE.isEmpty()) {
-                    if(IN_QUEUE.contains('\n')) {
-                        // consume one line
-
-                        Character character = null;
-                        while((character = IN_QUEUE.remove()) != null && character != '\n')
-                            text.append(character);
-
-                        return text.toString();
-                    }
-                }
-                Thread.sleep(100);
-            }
-            return "";
+            // todo: find a not stupid way of having a timeout...
+            char c = ' ';
+            while((c = in.readChar()) != '\n')
+                text.append(c);
+            return text.toString();
         } catch (Throwable e) {
+            connected = false;
             PUDSCLI.exception(e);
             throw new RuntimeException(e);
         }
     }
+    private boolean readPrompt() throws ExecutionException, InterruptedException {
+        return exec.submit(() -> {
+            return in.readChar() == '>';
+        }).get();
+    }
 
-    public String writeAndRead(String text) {
-        writeCommand(text);
-        return readReply();
+    public void safeWriteAndIgnore(String text) {
+        try {
+            exec.submit(() -> rawWriteAndIgnore(text)).get();
+        } catch (Throwable e) {
+            PUDSCLI.exception(e);
+            connected = false;
+        }
+    }
+
+    public Future<String> safeRead() {
+        return exec.submit(this::btRead);
+    }
+
+    public Future<String> safeWriteAndRead(String text) {
+        try {
+            return exec.submit(() -> {
+                rawWriteAndIgnore(text);
+                return btRead();
+            });
+        } catch (Throwable e) {
+            PUDSCLI.exception(e);
+            connected = false;
+        }
+        return null;
+    }
+
+    public void rawWriteAndIgnore(String text) {
+        btWrite(text);
+    }
+
+    public String rawWriteAndRead(String text) {
+        rawWriteAndIgnore(text);
+        return btRead();
     }
 
     private void inquireSpecific(RemoteDevice device) {
+        if(AVAILABLE.containsKey(device))
+            return;
         try {
             selected = device;
             AGENT.searchServices(null, SERVICES, selected, this);
@@ -200,31 +250,38 @@ public class ELM327Adapter extends AbstractAdapter implements DiscoveryListener 
 
     @Override
     public boolean isInitialized() {
-        return initialized && connected && READ_QUEUE != null && READ_QUEUE.isAlive();
+        return initialized && connected;
     }
 
     @Override
     public void selectECU(String code) {
         String[] split = code.split(":");
-        String txh = split[0];
-        String rxh = split[1];
+        selectedECUtx = split[0];
+        selectedECUrx = split[1];
 
-        String resp;
-        if((resp = writeAndRead("ATSH" + txh)).equals("?"))
-            throw new RuntimeException("Failed changing TXH (1)");
-        if((resp = writeAndRead("ATCRA" + rxh)).equals("?"))
-            throw new RuntimeException("Failed changing RXH (1)");
-        if((resp = writeAndRead("ATFCSH" + txh)).equals("?"))
-            throw new RuntimeException("Failed changing TXH (2)");
-        if((resp = writeAndRead("ATFCSD300000")).equals("?"))
-            throw new RuntimeException("Failed changing flow control data");
-        if((resp = writeAndRead("ATFCSM1")).equals("?"))
-            throw new RuntimeException("Failed changing flow control mode");
+        try {
+            exec.submit(() -> {
+                String resp;
+                if((resp = rawWriteAndRead("ATSH" + selectedECUtx)).equals("?"))
+                    throw new RuntimeException("Failed changing TXH (1)");
+                if((resp = rawWriteAndRead("ATCRA" + selectedECUrx)).equals("?"))
+                    throw new RuntimeException("Failed changing RXH (1)");
+                if((resp = rawWriteAndRead("ATFCSH" + selectedECUtx)).equals("?"))
+                    throw new RuntimeException("Failed changing TXH (2)");
+                if((resp = rawWriteAndRead("ATFCSD300000")).equals("?"))
+                    throw new RuntimeException("Failed changing flow control data");
+                if((resp = rawWriteAndRead("ATFCSM1")).equals("?"))
+                    throw new RuntimeException("Failed changing flow control mode");
+            }).get();
+        } catch (Throwable e) {
+            connected = false;
+            PUDSCLI.exception(e); // todo kill the connection on such conditions
+        }
     }
 
     @Override
     public Future<String> sendUDS(String uds) {
-        return exec.submit(() -> writeAndRead(uds));
+        return exec.submit(() -> rawWriteAndRead(uds));
     }
 
     @Override
